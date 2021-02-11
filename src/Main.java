95b2914cb824478a233ef6381b1e6eeced30164a
Tatyana Kopina <tatyanakopina@gmail.com>
2021-02-11 20:32:27 +0300

    Added REPORT.md & Changes in code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int current = 2_000_000_000;
-        int replenishment = 500_000_000;
-        int total = current + replenishment;
+        long current = 2_000_000_000;
+        long replenishment = 500_000_000;
+        long total = current + replenishment;
         System.out.println(total);
 
     }
