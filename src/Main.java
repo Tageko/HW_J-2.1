e30ccf0d7b767a6b6249774026779985e622f8f1
Tatyana Kopina <tatyanakopina@gmail.com>
2021-02-11 19:37:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int current = 2_000_000_000;
+        int replenishment = 500_000_000;
+        int total = current + replenishment;
+        System.out.println(total);
+
+    }
+}
